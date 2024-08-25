cf8ce446e0c38e6447150033d26be47fdc03a311
makatymba2001 <makatymba2001@yandex.ru>
2024-08-25 15:52:40 +0300

    Steamodded mod definition

---
+++ steamodded.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Handy
+--- MOD_ID: Handy
+--- MOD_AUTHOR: [SleepyG11]
+--- MOD_DESCRIPTION: Quality of Life controls for Balatro
+
+--- PRIORITY: 0
+--- DISPLAY_NAME: Handy
+--- PREFIX: handy
+--- VERSION: 1.0.3
+----------------------------------------------
+------------MOD CODE -------------------------
+
+----------------------------------------------
+------------MOD CODE END----------------------
