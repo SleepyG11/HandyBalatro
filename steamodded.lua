13faf80cd620ed02d2de7afec3e09aa914323231
makatymba2001 <makatymba2001@yandex.ru>
2024-10-01 01:23:54 +0300

    Updated version

--- steamodded.lua
+++ steamodded.lua
@@ -7,7 +7,7 @@
 --- PRIORITY: 0
 --- DISPLAY_NAME: Handy
 --- PREFIX: handy
---- VERSION: 1.1.0-pre-2
+--- VERSION: 1.1.0-pre.2
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
