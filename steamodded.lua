2a50f53f2ba8d637ae622d12472246ed25a4d778
makatymba2001 <makatymba2001@yandex.ru>
2025-01-24 05:11:45 +0200

    Fix #8

--- steamodded.lua
+++ steamodded.lua
@@ -7,7 +7,7 @@
 --- PRIORITY: 0
 --- DISPLAY_NAME: Handy
 --- PREFIX: handy
---- VERSION: 1.2.0a
+--- VERSION: 1.2.0b
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
