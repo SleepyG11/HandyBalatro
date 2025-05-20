ba379f22dc845a0489edb0a0622338feffd9a28f
makatymba2001 <makatymba2001@yandex.ru>
2025-05-20 14:38:02 +0300

    Fix selecting cards during dragging

--- index.lua
+++ index.lua
@@ -1820,6 +1820,7 @@ Handy.insta_highlight = {
 		return G.STATE ~= G.STATES.HAND_PLAYED
 			and card
 			and card.area == G.hand
+			and not G.CONTROLLER.dragging.target
 			and Handy.controller.is_module_key_down(Handy.cc.insta_highlight)
 	end,
 	execute = function(card)
@@ -1827,10 +1828,21 @@ Handy.insta_highlight = {
 			Handy.insta_highlight.first_card_highlighted = card.highlighted
 				and Handy.controller.is_module_enabled(Handy.cc.insta_highlight.allow_deselect)
 		end
-		if not not card.highlighted == not not Handy.insta_highlight.first_card_highlighted then
-			card:click()
-			return false
-		end
+		G.E_MANAGER:add_event(Event({
+			trigger = "immediate",
+			blocking = false,
+			blockable = false,
+			no_delete = true,
+			func = function()
+				if
+					not G.CONTROLLER.dragging.target
+					and (not not card.highlighted == not not Handy.insta_highlight.first_card_highlighted)
+				then
+					card:click()
+				end
+				return true
+			end,
+		}))
 		return false
 	end,
 
