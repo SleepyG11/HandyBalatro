ddc6415f474e908b63f2b9810d5f50d21b60cdd5
makatymba2001 <makatymba2001@yandex.ru>
2025-06-02 00:57:19 +0300

    Bump version

--- index.lua
+++ index.lua
@@ -7,7 +7,7 @@ to_big = to_big or function(x)
 end
 
 Handy = setmetatable({
-	version = "1.5.0-pre4",
+	version = "1.5.0-pre5",
 
 	last_clicked_area = nil,
 	last_clicked_card = nil,
--- steamodded.lua
+++ steamodded.lua
@@ -7,7 +7,7 @@
 --- PRIORITY: 0
 --- DISPLAY_NAME: Handy
 --- PREFIX: handy
---- VERSION: 1.5.0-pre4
+--- VERSION: 1.5.0-pre5
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
