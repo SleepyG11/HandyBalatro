9e07675b61ae6f2d7befa87e8f26728ac321f6b3
makatymba2001 <makatymba2001@yandex.ru>
2025-06-18 21:47:03 +0300

    Update metadata for 1.5.0

--- steamodded.lua
+++ steamodded.lua
@@ -2,12 +2,12 @@
 --- MOD_NAME: Handy
 --- MOD_ID: Handy
 --- MOD_AUTHOR: [SleepyG11]
---- MOD_DESCRIPTION: Quality of Life controls for Balatro
+--- MOD_DESCRIPTION: Quality of Life controls & keybinds
 
 --- PRIORITY: 0
 --- DISPLAY_NAME: Handy
 --- PREFIX: handy
---- VERSION: 1.5.0-rc3
+--- VERSION: 1.5.0
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
