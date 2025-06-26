14c3c7674a820444cf02a6074bc1f8ddda68db84
makatymba2001 <makatymba2001@yandex.ru>
2025-06-27 00:59:21 +0300

    Bump version

--- index.lua
+++ index.lua
@@ -4,7 +4,7 @@ end
 
 if not Handy then
 	Handy = setmetatable({
-		version = "1.5.0b",
+		version = "1.5.1",
 
 		last_clicked_area = nil,
 		last_clicked_card = nil,
--- steamodded.lua
+++ steamodded.lua
@@ -7,7 +7,7 @@
 --- PRIORITY: 0
 --- DISPLAY_NAME: Handy
 --- PREFIX: handy
---- VERSION: 1.5.0b
+--- VERSION: 1.5.1
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
